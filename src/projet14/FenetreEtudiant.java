c80b90b27d4ec65fab809935d0f5bf061c6d925f
Alph <Alph@Alphonsine>
2017-04-20 19:25:53 +0200

    hello

---
+++ src/projet14/FenetreEtudiant.java
@@ -0,0 +1,179 @@
+package projet14;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.border.Border;
+
+import graphisme.PoinDialog;
+
+
+public class FenetreEtudiant extends JFrame implements ActionListener{
+	public JLabel nom,prenom,sexe,etablisement,matricule,uefond,ueopt;//implémentation de l'interface proprement dite
+	public JTextField nom1,prenom1,matricule1;
+	private JButton reinit,envoye;//ainsi que des button et case à cocher
+	public JComboBox sexe3,etablis,uefond1,ueopt1;
+	
+	
+	public static void main(String [] args)//création d'une instance
+	{
+	FenetreEtudiant p=new FenetreEtudiant();
+	}
+
+	public FenetreEtudiant() //constructeur
+	{
+		setBounds(10,10,400,300);//spécification des coordonnées largeur 400,hauteur 300
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//opération par défaut quiter
+
+		Container c;
+		c=getContentPane();
+
+		BorderLayout bl=new BorderLayout();
+		c.setLayout(bl);
+		
+
+		JPanel p=new JPanel();
+		p.setLayout(null);
+
+		nom= new JLabel("NOM : ");
+		nom.setBounds(10,20,100,20);
+		p.add(nom);
+		
+		prenom= new JLabel("PRENOM : ");
+		prenom.setBounds(10,50,100,20);
+		p.add(prenom);
+		
+		sexe= new JLabel("SEXE : ");
+		sexe.setBounds(10,90,100,20);//spécification des coordonnées
+		p.add(sexe);
+		
+		etablisement= new JLabel("ENTABLISEMENT : ");
+		etablisement.setBounds(10,120,100,20);//spécification des coordonnées
+		p.add(etablisement);
+		
+		matricule= new JLabel("MATRICULE : ");
+		matricule.setBounds(10,150,100,20);
+		p.add(matricule);
+		
+		
+		uefond= new JLabel("UE FOND : ");
+		uefond.setBounds(10,180,100,20);
+		p.add(uefond);
+		
+		ueopt= new JLabel("UE OPT : ");
+		ueopt.setBounds(10,210,170,20);
+		p.add(ueopt);
+		
+		nom1=new JTextField(20);
+		nom1.setBounds(120,20,100,20);
+		p.add(nom1);
+		
+		prenom1=new JTextField(20);
+		prenom1.setBounds(120,50,100,20);
+		prenom1.setEditable(true);
+		p.add(prenom1);
+		
+		Object[] elements = new Object[]{"M", "F"};
+		sexe3=new JComboBox(elements);
+		sexe3.setBounds(120,90,100,20);
+		sexe3.setEditable(true);
+		p.add(sexe3);
+		
+		Object[] elements1 = new Object[]{"FS", "FSJP","FASEG","FALSH"};
+		etablis=new JComboBox(elements1);
+		etablis.setBounds(120,120,100,20);
+		etablis.setEditable(true);
+		p.add(etablis);
+		
+		matricule1=new JTextField(20);
+		matricule1.setBounds(120,150,100,20);
+		matricule1.setEditable(true);
+		p.add(matricule1);
+		
+		Object[] elements2 = new Object[]{"Atelier de programmation"
+				, "Algorithmique 2","Systeme Informatique"};
+		uefond1=new JComboBox(elements2);
+		uefond1.setBounds(120,180,100,20);
+		p.add(uefond1);
+		
+		Object[] elements3 = new Object[]{"Méthode/Complément"
+				};
+		ueopt1=new JComboBox(elements3);
+		ueopt1.setBounds(120,210,100,20);
+		p.add(ueopt1);
+		
+//		editDis=new JTextField(20);
+//		editDistance.setBounds(120,170,150,20);
+//		editDistance.setEditable(true);
+//		p.add(editDistance);
+		
+		Border bord=BorderFactory.createRaisedBevelBorder();
+		
+		envoye=new JButton("Envoyer");
+		envoye.addActionListener(this);
+		envoye.setBorder(bord);
+		envoye.setBounds(240,35,100,20);
+		p.add(envoye);
+		
+		reinit=new JButton("Réinitialiser");
+		reinit.setBorder(bord);
+		reinit.setBounds(240,105,100,20);
+		p.add(reinit);
+		
+		
+		
+		c.add(p, BorderLayout.CENTER);
+		setVisible(true);
+		
+		reinit.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				nom1.setText(null);
+				prenom1.setText(null);
+				matricule1.setText(null);
+				
+			}
+		});
+		envoye.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				
+	NoteUE h=new codeA(nom1.getText(), prenom1.getText(),matricule.getText(),sexe3.getSelectedItem());
+	h.setVisible(true);
+				
+				
+			}
+		});
+   }
+	
+	
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+	
+}
+		
+
+		
+
+	
+	
+		
+		
+
+
---
+++ src/projet14/NoteUE.java
@@ -0,0 +1,199 @@
+package projet14;
+
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
+
+import graphisme.Point;
+
+
+public class NoteUE extends JDialog implements ActionListener {
+	
+	JButton envoyer,reinit;
+	JLabel uefond,note,credit,code1,code2,code3,ueopt,notea,credita,code4;
+	JTextField note1,credit1,note2,credit2,note3,credit3,note4,credit4;
+	FenetreEtudiant k=new FenetreEtudiant();
+	
+	public NoteUE(String n,String p,String v ){
+		
+	}
+	
+	public NoteUE(ActionListener f,String s){
+    
+		super();
+    setBounds(500,100,400,300);
+    setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
+    
+    Container c=getContentPane();
+    c.setLayout(null);
+    
+    uefond=new JLabel("UE FOND :");
+    uefond.setBounds(10,20,120,20);
+    c.add(uefond);
+    
+    code1=new JLabel("CODE 1 :");
+    code1.setBounds(10,50,120,20);
+    c.add(code1);
+    
+    code2=new JLabel("CODE 2 :");
+    code2.setBounds(10,80,120,20);
+    c.add(code2);
+    
+    code3=new JLabel("CODE 3 :");
+    code3.setBounds(10,110,120,20);
+    c.add(code3);
+    
+    note=new JLabel("NOTE :");
+    note.setBounds(140,20,110,20);
+    c.add(note);
+    
+    ueopt=new JLabel("UE OPT :");
+    ueopt.setBounds(10,140,120,20);
+    c.add(ueopt);
+    
+    code4=new JLabel("CODE 4 :");
+    code4.setBounds(10,170,120,20);
+    c.add(code4);
+    
+
+    
+    credit1=new JTextField();
+    credit1.setBounds(200,50,80,20);
+    c.add(credit1);
+    
+    credit2=new JTextField();
+    credit2.setBounds(200,80,80,20);
+    c.add(credit2);
+    
+    credit3=new JTextField();
+    credit3.setBounds(200,110,80,20);
+    c.add(credit3);
+    
+    credit4=new JTextField();
+    credit4.setBounds(200,170,80,20);
+    c.add(credit4);
+    
+    note1=new JTextField();
+    note1.setBounds(110,50,80,20);
+    c.add(note1);
+    
+    note2=new JTextField();
+    note2.setBounds(110,80,80,20);
+    c.add(note2);
+    
+    note3=new JTextField();
+    note3.setBounds(110,110,80,20);
+    c.add(note3);
+    
+    note4=new JTextField();
+    note4.setBounds(110,170,80,20);
+    c.add(note4);
+    
+    notea=new JLabel("NOTE :");
+    notea.setBounds(110,140,120,20);
+    c.add(notea);
+    
+    credita=new JLabel("CREDIT :");
+    credita.setBounds(200,140,120,20);
+    c.add(credita);
+    
+    credit=new JLabel("CREDIT :");
+    credit.setBounds(200,20,120,20);
+    c.add(credit);
+    
+	
+	Border bord=BorderFactory.createRaisedBevelBorder();
+	envoyer=new JButton("ENVOYER");
+	envoyer.setActionCommand("ENVOYER");
+	envoyer.addActionListener(this);
+	envoyer.setBorder(bord);
+	envoyer.setBounds(300,150,80,30);
+	c.add(envoyer);
+	
+	reinit=new JButton("REINITIALISER");
+	reinit.setActionCommand("ENVOYER");
+	reinit.addActionListener(this);
+	//Cancel.setBorder(bord);
+	reinit.setBounds(300,100,80,30);
+	c.add(reinit);
+	
+	reinit.addActionListener(new ActionListener() {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			note1.setText(null);
+			note2.setText(null);
+			note3.setText(null);
+			note4.setText(null);
+			credit1.setText(null);
+			credit2.setText(null);
+			credit3.setText(null);
+			credit4.setText(null);
+			
+		}
+	});
+	
+	envoyer.addActionListener(new ActionListener() {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			 String noteue1,noteue2,noteue3,noteue4;
+			 String creditue1,creditue2,creditue3,creditue4;
+			 creditue1=credit1.getText();
+			 creditue2=credit2.getText();
+			 creditue3=credit3.getText();
+			 creditue4=credit4.getText();
+			 
+			 noteue1=(note1.getText());
+		     noteue2=(note2.getText());
+		     noteue3=(note3.getText());
+		     noteue4=(note4.getText());
+			 
+			 Double c1,c2,c3,c4,c;
+			 Double n1,n2,n3,n4,s,m;
+			 c1=Double.parseDouble(creditue1);
+			 c2=Double.parseDouble(creditue2);
+			 c3=Double.parseDouble(creditue3);
+			 c4=Double.parseDouble(creditue4);
+			 
+			 n1=Double.parseDouble(noteue1);
+			 n2=Double.parseDouble(noteue2);
+			 n3=Double.parseDouble(noteue3);
+			 n4=Double.parseDouble(noteue4);
+			 
+			 s=n1*c1+n2*c2+n3*c3+n4*c4;
+			 c=c1+c2+c3+c4;
+			 m=s/c;
+			 
+			 FenetreEtudiant k=new FenetreEtudiant();
+			
+			 String n=k.nom1.getText();
+			 String p=k.prenom1.getText();
+			 String j=k.matricule1.getText();
+			 String v=k.sexe3.getActionCommand();
+			JOptionPane pane= new JOptionPane();
+			
+			pane.showMessageDialog(null, "L'étudiant "+n+" "+p+" sexe "
+			+v+" de  matricule "+j+" ayant composé les UEs "+" "
+					+ "a eu une moyenne de "+ m);
+			
+		}
+	});
+	
+	}
+	
+	public void actionPerformed(ActionEvent e){
+		
+	}
+	
+}
